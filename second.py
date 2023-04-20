ff1df9c1c16c7a327cb45c197569a25633156389
hryszyx <hryszyx@gmail.com>
2023-04-20 19:35:23 +0800

    * 初步优化小数据集

--- second.py
+++ second.py
@@ -1,22 +1,16 @@
+import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from normalization import max_min_normalization
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.pipeline import FeatureUnion
-from sklearn.preprocessing import StandardScaler
-from util import DataFrameSelector
 from dataClean import initData
-from regression import regressionAndAnalysis
-# from chemicalDescriptor import chemicDescriptorData_prepare
 from dataClean import data_wash
-from regression import regressionAndAnalysis
+from regression import *
 from DataPreprocessing import *
+from util import *
 
-from sklearn.preprocessing import QuantileTransformer
-
+warnings.filterwarnings('ignore')
 
 # 读文件
 file = pd.read_excel("database\OUT_GCMC_fur_tip5.xlsx")
@@ -27,57 +21,63 @@ file = pd.read_excel("database\OUT_GCMC_fur_tip5.xlsx")
 # pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
 # 去重
-# file.drop_duplicates(inplace=True)
-# file.reset_index(drop=True, inplace=True)
+file.drop_duplicates(inplace=True)
+file.reset_index(drop=True, inplace=True)
 
 # 初始化
 feature = ['LCD', 'PLD', 'LFPD', 'cm3_g',
            'ASA_m2_cm3', 'ASA_m2_g', 'AV_VF', 'AV_cm3_g']
 # labels = ["Hr_fur/tip5"]
-labels = ['Henry_furfural', "Henry_ethanol", 'Henry_Tip5p', 'Heat_furfural', 'Heat_Tip5p',"Hr_fur/tip5", "Hr_eth/tip5"]
+labels = ['Henry_furfural', "Henry_ethanol", 'Henry_Tip5p',
+          'Heat_furfural', 'Heat_Tip5p', "Hr_fur/tip5", "Hr_eth/tip5"]
 
 column = feature + labels
 
 dataset = file[column]
 
-# 去空
-# dataset.dropna(inplace=True)
-
 # 清洗
 for item in labels:
     dataset = data_wash(dataset, item)
 
-dataset_labels = dataset[labels]
-dataset_select = dataset[feature]
+dataset['Henry_furfural'] = np.log(dataset['Henry_furfural']+1)
+dataset['Henry_Tip5p'] = np.log(dataset['Henry_Tip5p']+1)
+dataset['Henry_ethanol'] = np.log(dataset['Henry_ethanol']+1)
+dataset['Hr_fur/tip5'] = np.log(dataset['Hr_fur/tip5']+1)
+dataset['Hr_eth/tip5'] = np.log(dataset['Hr_eth/tip5']+1)
 
-dataset_labels['Henry_furfural'] = np.log(dataset_labels['Henry_furfural']+1)
-dataset_labels['Henry_Tip5p'] = np.log(dataset_labels['Henry_Tip5p']+1)
-dataset_labels['Henry_ethanol'] = np.log(dataset_labels['Henry_ethanol']+1)
 
-dataset_labels['Hr_fur/tip5'] = np.log(dataset_labels['Hr_fur/tip5']+1)
-# dataset_labels['Hr_eth/tip5'] = np.log(dataset_labels['Hr_eth/tip5']+1)
- 
-#移除低方差特征
+# 回归分析散点图，展示不同变量之间的关系
+
+for i in labels:
+    whatData(features=feature, label=i, dataset=dataset)
+
+# #目标值可视化
+plt.plot(dataset['Heat_furfural'])
+plt.show()
+
+plt.plot(dataset['Henry_furfural'])
+plt.show()
+
+# 移除低方差特征
 # from sklearn.feature_selection import VarianceThreshold
 # sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
 # sel.fit_transform(dataset_select)
 
-X_train, X_test, Y_train, Y_test = split(
-    dataset_select, dataset_labels, test_size=0.2)
+# 真正想训练的内容
+for i in labels:
+    true_label = [i]
+    dataset_select = dataset[feature]
+    dataset_labels = dataset[true_label]
+    X_train, X_test, Y_train, Y_test = preprocessing(
+        dataset_select, dataset_labels, test_size=0.2)
+    singleRA(true_label, "model_RandomForestRegressor",
+             X_train, Y_train, X_test, Y_test, dataset_select)
+    # singleRAWithDiffModel(true_label, X_train,Y_train, X_test, Y_test, dataset_select)
 
-#归一化特征
-# from sklearn.preprocessing import StandardScaler
-# std = StandardScaler()
-# scaler= std.fit(X_train)
-# X_train=scaler.transform(X_train)
-# X_test_scaler=scaler.transform(X_test)
 
-# 数据预处理
-X_train, X_test = imp(X_train, X_test)
-X_train, X_test = scale(X_train, X_test)
-Y_train, Y_test = normal_qt(Y_train, Y_test)
-# Y_train, Y_test = scale(Y_train, Y_test)
+# # 数据预处理
+# X_train, X_test, Y_train, Y_test = preprocessing(dataset_select, dataset_labels, test_size=0.2)
 
-# 训练分析
-regressionAndAnalysis(labels, "model_RandomForestRegressor",
-                      X_train, Y_train, X_test, Y_test)
+# # 训练分析
+# regressionAndAnalysis(labels, "model_RandomForestRegressor",
+#                       X_train, Y_train, X_test, Y_test)
--- third.py
+++ third.py
@@ -90,12 +90,13 @@ dataset_labels['selectivity_of_Henry'] = np.log(
 # regressionAndAnalysis(labels, "model_RandomForestRegressor",
 #                       X_train, Y_train, X_test, Y_test, dataset_select)
 
+print(dataset.info)     #10098
 
 # 真正想训练的内容
-for i in labels:
-    true_label = [i]
-    dataset_select = dataset[feature]
-    dataset_labels = dataset[true_label]
-    X_train, X_test, Y_train, Y_test = preprocessing(dataset_select, dataset_labels, test_size=0.2)
-    singleRA(true_label, "model_RandomForestRegressor", X_train,Y_train, X_test, Y_test, dataset_select)
-    # singleRAWithDiffModel(true_label, X_train,Y_train, X_test, Y_test, dataset_select)
+# for i in labels:
+#     true_label = [i]
+#     dataset_select = dataset[feature]
+#     dataset_labels = dataset[true_label]
+#     X_train, X_test, Y_train, Y_test = preprocessing(dataset_select, dataset_labels, test_size=0.2)
+#     singleRA(true_label, "model_RandomForestRegressor", X_train,Y_train, X_test, Y_test, dataset_select)
+#     # singleRAWithDiffModel(true_label, X_train,Y_train, X_test, Y_test, dataset_select)
